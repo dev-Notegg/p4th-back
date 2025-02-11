1815287270798e0317c7a38f24340bdc4bfa8cd2
yerin0408 <pqlaowks0247@gmail.com>
2025-02-11 16:55:55 +0900

    ParameterObject 변경

--- src/main/java/com/p4th/backend/controller/PostController.java
+++ src/main/java/com/p4th/backend/controller/PostController.java
@@ -13,7 +13,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.api.annotations.ParameterObject;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
--- src/main/java/com/p4th/backend/controller/SearchController.java
+++ src/main/java/com/p4th/backend/controller/SearchController.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.api.annotations.ParameterObject;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
