71f5f74e376aaf44e10ceeaf64c5108ef6aaa1fe
yerin0408 <pqlaowks0247@gmail.com>
2025-03-14 09:42:35 +0900

    내가 쓴 댓글 목록 조회 수정

--- src/main/java/com/p4th/backend/dto/response/user/UserCommentResponse.java
+++ src/main/java/com/p4th/backend/dto/response/user/UserCommentResponse.java
@@ -13,9 +13,6 @@ public class UserCommentResponse {
     @Schema(description = "댓글 작성자 ID")
     private String userId;
 
-    @Schema(description = "댓글 작성자 닉네임")
-    private String nickname;
-
     @Schema(description = "게시글 작성자 ID")
     private String writerLoginId;
 
@@ -29,7 +26,6 @@ public class UserCommentResponse {
         UserCommentResponse dto = new UserCommentResponse();
         dto.setCommentId(comment.getCommentId());
         dto.setUserId(comment.getUserId());
-        dto.setNickname(comment.getNickname() != null ? comment.getNickname() : "");
         dto.setContent(comment.getContent());
         dto.setCreatedAt(comment.getCreatedAt() != null ?
                 RelativeTimeFormatter.formatRelativeTime(comment.getCreatedAt()) : null);
